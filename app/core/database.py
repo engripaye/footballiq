a019eba44f5605d4bf59bdce76e60ec803716509
mac <@b.tunde.ipaye@gmail.com>
2026-07-10 16:43:50 +0100

    backend service commit by ENGR. IPAYE

--- app/core/database.py
+++ app/core/database.py
@@ -1,15 +1,22 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine)
+    bind=engine
+)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
