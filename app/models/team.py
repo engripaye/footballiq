b25690bed65ee1f629286944611e52e858cdce9a
mac <@b.tunde.ipaye@gmail.com>
2026-07-10 02:26:24 +0100

    backend service commit by ENGR. IPAYE

---
+++ app/models/team.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Float
+from app.core.database import Base
+
+class Team(Base):
+    __tablename__ = "team"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, unique=True)
+    country = Column(String, nullable=False)
+    league = Column(String, nullable=False)
+
+    elo_rating = Column(Float, default=1500)
+    attacking_strength = Column(Float, default=1.0)
+    defense_strength = Column(Float, default=1.0)
+
+    form_score = Column(Float, default=50_0)
